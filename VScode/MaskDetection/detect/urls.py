7ba32f9d8cc3b85ef8e32edb42a4e82b360d61c8
Green0v0 <greenwint3r@gmail.com>
2021-06-02 15:37:42 +0900

    add files

---
+++ VScode/MaskDetection/detect/urls.py
@@ -0,0 +1,12 @@
+# 2차 url
+from django.urls import path
+# from .views import *
+from . import views
+
+# 이 url pattern들의 namespace(prefix)로 사용할 값 설정
+# urlpattern 설정의 이름 호출 시 다른 app들과 구분하기 위해 사용한다.
+app_name = "detect"
+
+urlpatterns = [
+
+]
---
+++ VScode/MaskDetection/detect/views.py
@@ -0,0 +1,6 @@
+# detect/views.py
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse # reverse함수 : path 설정 이름으로 url 문자열을 만들어 주는 함수
+
+# Create your views here.
